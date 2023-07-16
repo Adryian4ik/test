ecc602fc3f16a0fec327ae701376ae1e603653dd
Adryian4ik <112876989+Adryian4ik@users.noreply.github.com>
2023-07-16 13:42:38 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,7 +9,7 @@ class ImageHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'image/jpeg')  # Замените на подходящий тип картинки
             self.end_headers()
-            with open('./api-datasets/SRTM_BLR.tif', 'rb') as image_file:  # Замените на путь к вашей картинке
+            with open('/home/adryian/api-datasets/SRTM_BLR.tif', 'rb') as image_file:  # Замените на путь к вашей картинке
                 self.wfile.write(image_file.read())
         else:
             self.send_response(404)
