3d91bed5f88b119e835bd132e1ebf2df2c576787
Adryian4ik <112876989+Adryian4ik@users.noreply.github.com>
2023-07-16 15:26:22 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,8 @@
-import http.server
-import socketserver
+import numpy as np
 
-PORT = 8000
+F = open("/home/adryian/api-datasets/SRTM_BLR.tif", "r")
 
-class ImageHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/image':
-            self.send_response(200)
-            self.send_header('Content-type', 'image/jpeg')  # Замените на подходящий тип картинки
-            self.end_headers()
-            with open('/home/adryian/api-datasets/SRTM_BLR.tif', 'rb') as image_file:  # Замените на путь к вашей картинке
-                self.wfile.write(image_file.read())
-        else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b'Not Found')
+A = np.array(F.read())
 
-with socketserver.TCPServer(('', PORT), ImageHandler) as httpd:
-    print(f'Serving at port {PORT}')
-    httpd.serve_forever()
+
+print(A)
