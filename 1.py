34387c923a24ef40618cc35107e04324019926bf
Adryian4ik <112876989+Adryian4ik@users.noreply.github.com>
2023-07-16 13:41:02 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -5,9 +5,9 @@ PORT = 8000
 
 class ImageHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == 'image':
+        if self.path == '/image':
             self.send_response(200)
-            self.send_header('Content-type', 'imagejpeg')  # Замените на подходящий тип картинки
+            self.send_header('Content-type', 'image/jpeg')  # Замените на подходящий тип картинки
             self.end_headers()
             with open('./api-datasets/SRTM_BLR.tif', 'rb') as image_file:  # Замените на путь к вашей картинке
                 self.wfile.write(image_file.read())
